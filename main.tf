49971c81c8ddedfd58a50b56e15f571931a05904
shaygx <shaygx@gmail.com>
2026-06-26 10:15:17 +0300

    migrate demo-app to production

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "kubernetes_namespace" "demo_namespace" {
 resource "kubernetes_deployment" "demo_app" {
   metadata {
     name      = "demo-app"
-    namespace = kubernetes_namespace.demo_namespace.metadata[0].name
+    namespace = "production"
   }
 
   spec {
@@ -21,13 +21,55 @@ resource "kubernetes_deployment" "demo_app" {
       }
       spec {
         container {
-          image = "nginx:alpine" # Placeholder until we build your image
+          # Replace with your actual image path later
+          image = "nginx:alpine" 
           name  = "demo-app"
+          port {
+            container_port = 80
+          }
         }
       }
     }
   }
 }
 
+resource "kubernetes_service" "demo_service" {
+  metadata {
+    name      = "demo-service"
+    namespace = kubernetes_namespace.demo_namespace.metadata[0].name
+  }
+  spec {
+    selector = { app = "demo-app" }
+    port {
+      port        = 80
+      target_port = 80
+    }
+  }
+}
 
-# Testing the Pipeline
+resource "kubernetes_ingress_v1" "demo_ingress" {
+  metadata {
+    name      = "demo-ingress"
+    namespace = "production"
+    annotations = {
+      "kubernetes.io/ingress.class" = "nginx"
+    }
+  }
+  spec {
+    rule {
+      host = "demoapp.local"
+      http {
+        path {
+          path      = "/"
+          path_type = "Prefix"
+          backend {
+            service {
+              name = "demo-service"
+              port { number = 80 }
+            }
+          }
+        }
+      }
+    }
+  }
+}
